a577854ea64e80e29f830ac12009aa77f8f1275c
Nedlees <Guimenes.Math@gmail.com>
2024-10-11 08:15:22 -0300

    Atualizando o footer do sobrenos.php

--- php/sobrenos.php
+++ php/sobrenos.php
@@ -116,31 +116,9 @@
     <br><br>
 
     <footer>
-        <br>
-        <div class="footer-wave"></div>
-        <div class="containerr">
-            <div class="footer-logo">
-                <img src="../Imagens/logo.png" alt="">
-            </div>
-            <div class="footer-content">
-                <div class="redes-sociais">
-                    <h3> Nos acompanhe nas redes sociais!</h3>
-                    <div class="icons">
-                        <a href="https://www.facebook.com/profile.php?id=61563278681181&mibextid=ZbWKwL"><img src="../Imagens/face.png" alt="Facebook"></a>
-                        <a href="#"><img src="../Imagens/what.png" alt="Whatsapp"></a>
-                        <a href="https://www.instagram.com/ajuda_petfinder?igsh=emI0aGxseG12ejV2"><img src="../Imagens/insta.png" alt="instagram"></a>
-                    </div>
-                </div>
-                <div class="telefones">
-                    <h3>Como encontrar o Petfinder?</h3>
-                    <p>Endereço: R. Aviador Gomes Ribeiro, 34-60 - P.Paulistano, Bauru - SP, 17030-530 <br><br></p>
-                    <p>Telefone:  (14) 3203-2553</p>
-                </div>
-                <div class="desenvolvimento">
-                    <p>Desenvolvido por Maria Eduarda</p>
-                </div>
-            </div>
-        </div>
+        <?php 
+        include'footer.php';
+        ?>
     </footer>
 </body>
 
