43a230bf7da41ddd06f57e4860ad2c80f2526557
Nedlees <Guimenes.Math@gmail.com>
2024-10-23 09:44:17 -0300

    Blog tartaruta html e css concluídos

--- php/Blog_tartaruga.php
+++ php/Blog_tartaruga.php
@@ -4,15 +4,137 @@ include "config.php"
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../css/blog_tartaruga.css">
   <title>Blog Tartarugas Domésticas</title>
 </head>
+
 <body>
-  
 
+  <header>
+    <?php 
+  include 'navbar.php';
+  ?>
+  </header>
 
+  <br>
+
+  <h1>Tatarugas Domésticas: o que você precisa saber</h1>
+
+  <div class="container-blog">
+    <img src="../Imagens/Tartaruga.jpeg" alt="Tartaruga">
+    <p class="texto">
+      Se você quer um animal dócil e bem tranquilo para se ter em casa, a tartaruga doméstica é uma ótima
+      escolha. Então, se quer fugir dos padrões de pets tradicionais, essa é a hora de descobrir como criá-la e as
+      principais características que encantam muitos tutores afora.</p>
+
+    <img src="../Imagens/Tartaruga Matheus.jpeg" alt="Tartaruga 2">
+
+    <p class="texto">
+      Por vezes confundido com outras espécies de quelônios, as tartarugas podem ser criadas como animais
+      domésticos, ao contrário dos “parentes” silvestres. No entanto, se você ainda acha que elas são a mesma coisa que
+      cágado ou jabuti, vamos te mostrar que não.
+<br><br>
+      É hora de entrar a fundo no reino animal e desvendar os mistérios sobre esse assunto. Vem com a gente e descubra
+      se tartaruga pode ser domesticada e qual é a melhor forma de cuidar desse pet.</p>
+<br>
+
+  <h3>Qual é a diferença entre tartaruga, cágado e jabuti?</h3>
+
+    <p class="texto">
+    Antes de mais nada, precisamos te contar que existem três espécies que são comumente confundidas, mas que são muito diferentes entre si: tartaruga, cágado e jabuti. Mas quais são as principais diferenças e quais desses bichos costumam ser domesticados? 
+    <br><br>
+    A tartaruga, por exemplo, se diferencia principalmente por conta do casco: ele é redondo, alto e duro. Já o pescoço é o mais curto dentre essas três espécies. Na natureza, ela gosta de passar mais tempo na água e se desenvolve bem no quesito nado. 
+    <br><br>
+    Uma das curiosidades sobre tartarugas domésticas é que elas costumam sair da água apenas para tomar sol ou desovar. Outro ponto é que um desses animais de estimação chegam a viver 40 anos com o tutor. 
+    <br><br>
+    Já o cágado, em seu habitat natural, vive bem tanto na água quanto na terra. Diferentemente da tartaruga doméstica, o casco tem formato achatado e mais oval, e o pescoço alongado. Não podemos esquecer das patas: elas possuem membranas entre os dedos para facilitar o nado, o que não existe nas tartarugas domésticas.
+    <br><br>
+    Por fim, o jabuti é o que mais se distancia em aparência da tartaruga. O casco dessa espécie é muito mais alto e mais pesado do que o das suas colegas tartaruga e cágado. Sua alimentação é essencialmente herbívora, e esse animal vive apenas em terra.</p>
+
+    <br>
+    
+    <h3>Quantas e quais espécies de tartaruga existem?</h3>
+
+    <p class="texto">Existem diversos tipos de tartarugas na natureza, mas não são todos que podemos criar em casa. Para entender melhor quais são as espécies de tartaruga doméstica, confira quatro tipos desse animal que você pode ter de estimação, sem problemas com o IBAMA.</p>
+
+    <img src="../Imagens/tartub.jpg" alt="Tartaruga 3">
+
+    <h4>Tartaruga Pintada</h4>
+
+    <p class="texto">
+    Animal pequeno, de cerca de 25 centímetros e que é considerado um cágado, embora tenha nome de tartaruga doméstica. Seguindo as características, o casco é rígido e com riscos amarelos. 
+    <br><br>
+    Por conta do tamanho, a alimentação precisa ser balanceada para evitar o sobrepeso. O ideal é oferecer rações balanceadas e próprias para tartaruga doméstica.
+    </p>
+
+    <h4>Tartaruga-da-madeira</h4>
+
+    <p class="texto">
+    Seu físico é bem colorido: o casco é cinza e o restante do corpo tem uma cor alaranjada e manchas escuras na região da cabeça e patas. Seu tamanho também é pequeno e a espécie mede em torno de 20 centímetros.
+    <br><br>
+    Pelo tamanho, é outro tipo de tartaruga doméstica que exige alimentação saudável e equilibrada. Nesse caso, o ideal é oferecer um misto de ração e frutas. 
+    </p>
+
+    <h4>Tartaruga-Russa</h4>
+
+    <p class="texto">
+    Essa espécie é diferente das demais por ter a extensão do corpo mais clara. O casco, no entanto, é escuro com manchas, formando um contraste bem interessante e bonito.
+    <br><br>
+    Também mede por volta dos 20 centímetros e costuma se alimentar de plantas. Essa é, portanto, uma das espécies mais tranquilas para se manter sem se preocupar com o sobrepeso.
+    </p>
+
+    <h4>Tartaruga-Tigre-d'água</h4>
+    
+    <p class="texto">
+    Essa é a última das melhores espécies de tartaruga para se ter em casa. É um dos tipos que se destaca mais porque tem aparência delicada e é muito pequeno quando filhote, medindo cerca de 5 centímetros.
+
+    Mas não se engane: ele não fica pequeno para sempre. Quando chega à fase adulta, o animal chega aos 30 centímetros. Outro ponto de destaque é a longevidade. Essa tartaruga doméstica chega a viver, em média, 30 ano
+    </p>
+
+    <h2>O que come uma tartaruga doméstica?</h2>
+
+    <p class="texto">
+    Muita gente se questiona sobre a <strong>alimentação de tartarugas domésticas</strong>. Como você viu nas espécies citadas acima, existem as que comem plantas, outras que se alimentam de frutas e outras, ainda, de ração balanceada específica.
+
+    As refeições desses animais podem ser muito variadas, mas se você não sabe o que oferecer, o melhor é procurar um profissional para te orientar. Isso porque a ingestão de alimentos indevidos pode levar à morte do seu pet. 
+
+    Por isso, consulte um veterinário de confiança para que ele identifique a espécie e prescreva a alimentação correta. Nesse ponto, vale lembrar, ainda, sobre as quantidades para evitar o sobrepeso. Mas os variados tipos de tartarugas podem comer alimentos como:
+    </p>
+    <p class="texto">
+      <li>Folhas;</li>
+      <li>Frutas;</li>
+      <li>Legumes e verduras em geral;</li>
+      <li>Carnes e minhocas;</li>
+      <li>Peixes e crustáceos;</li>
+      <li>Insetos</li>
+    </p>
+
+    <h2>Como cuidar de uma tartaruga em casa?</h2>
+
+    <p class="texto">Até aqui você viu alguns cuidados com tartaruga doméstica, especialmente em relação à alimentação. Mas não é só isso que importa na hora de adotar um animal dessa espécie.
+<br><br>
+    Apesar de parecer um pet preguiçoso, bem diferente dos agitados cães e gatos, as tartarugas precisam de um espaço só delas. Sempre considere o tamanho máximo que a espécie que você escolheu vai atingir. 
+<br><br>
+    Em tanques ou aquários, o ideal é que o tutor reserve cerca de um terço de área terrestre e o restante para natação, sempre com iluminação ultravioleta de espectro total. Isso faz com que o local fique em uma temperatura agradável para o pet.
+<br><br>
+    Lembre-se também de sempre fazer a manutenção do local. A água limpa impede a proliferação de fungos e microrganismos, preserva a saúde do animal e, claro, faz com que a tartaruga doméstica reconheça aquele como seu ambiente favorito.
+<br><br>
+    Quando o assunto é cuidar bem do seu pet, a gente sabe de tudo! No site da Petz, os tutores têm diversidade de brinquedos, casinhas, alimentos e tudo para cuidar do bem-estar do seu melhor amigo. Que tal já escolher um aquário para a tartaruga doméstica com a gente? Estamos te esperando!
+  </p>
+
+  </div>
+
+<p class="referencia">Referência: <strong><a href="https://www.petz.com.br/blog/bem-estar/tartaruga-domestica/">Petz.com.br</a></strong></p>
+
+  <footer>
+    <?php 
+  include 'footer.php';
+  ?>
+  </footer>
 </body>
+
 </html>
